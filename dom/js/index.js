36f735703d6a64bf73afdaf40c23581a69e9460c
Joel Ross <joelross@uw.edu>
2026-04-27 16:19:58 -0700

    Add dom demo starter code

---
+++ dom/js/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const songArray = [
+  { artist: "Queen", title: "Don't Stop Me Now", youtubeUrl: "https://www.youtube.com/watch?v=HgzGwKwLmgM" },
+  { artist: "David Bowie", title: "Starman", youtubeUrl: "https://www.youtube.com/watch?v=rpO1U-nEgRU"
+  },
+  { artist: "HUNTR/X", title: "Golden", youtubeUrl: "https://www.youtube.com/watch?v=yebNIHKAC4A"
+  },
+];
