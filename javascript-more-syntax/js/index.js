af39775948039b2c3da4708f39a807cc352b4c06
Joel Ross <joelross@uw.edu>
2026-04-30 15:15:43 -0700

    End of es6 demo

--- javascript-more-syntax/js/index.js
+++ javascript-more-syntax/js/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
-//console.log("executing main.js")
+import { sayHello, password } from './other.js'
+//import otherFunction from './other.js';
+
+import Button from './other.js';
+
+console.log("executing main.js")
 
 //an array of objects to work with
 const peopleArray = [
@@ -10,4 +15,48 @@ const peopleArray = [
   {name: 'Diya', height: 69, weight: 144},
   {name: 'Emma', height: 71, weight: 152}
 ]
-//console.log(peopleArray);
+console.log(peopleArray);
+
+
+sayHello();
+console.log("I know your password: " + password);
+
+//otherFunction();
+
+
+// const foo = (params) => {
+//   console.log("foo" + params);
+// }
+
+
+// const myArray = [1, 2, 3];
+// const [x, y, z, a] = myArray;
+// // const x = myArray[0];
+// // const y = myArray[1];
+// // const z = myArray[2];
+// console.log(x); //=> 1;
+// console.log(y); //=> 2;
+// console.log(z); //=> 3;
+// console.log(a); //=> 3;
+
+
+// const myObject = {a: 1, b: 2, c: 3};
+// const {a, c, b, d} = myObject;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// function getBMI(personObj) {
+//   const {height, weight} = personObj;
+  
+//   return 703*weight/(height*height);
+// }
+
+
+// const originalArray = ['a', 'b', 'c', 'd'];
+
+// const newArray = ['e,', ...originalArray, 'e']
+
+// console.log(newArray);
+
--- javascript-more-syntax/js/other.js
+++ javascript-more-syntax/js/other.js
@@ -1,3 +1,20 @@
 'use strict';
 
 console.log("executing other.js");
+
+const peopleArray = ["John", "Paul", "George", "Ringo"];
+console.log(peopleArray);
+
+export function sayHello() {
+  console.log("hello");
+}
+
+// export default function otherDefault() {
+//   console.log("Other is best module!");
+// }
+
+export const password = "a secret";
+
+export default function Button() {
+
+}
