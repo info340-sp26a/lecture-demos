016daa7c8cac5b7e9feae4087eac43a8a8ebf5ad
Joel Ross <joelross@uw.edu>
2026-06-02 08:42:01 -0700

    End of firebase db demo

--- react-chat-firebase-1/src/components/App.jsx
+++ react-chat-firebase-1/src/components/App.jsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 
 import { Routes, Route, Outlet, Navigate, useNavigate } from 'react-router';
 
+import { getDatabase, ref, set as firebaseSet, push as firebasePush, onValue } from 'firebase/database';
+
 import { HeaderBar } from './HeaderBar.jsx';
 
 import ChatPage from './ChatPage.jsx';
@@ -12,10 +14,38 @@ import INITIAL_CHAT_LOG from '../data/chat_log.json'
 import DEFAULT_USERS from '../data/users.json';
 
 function App(props) {
-  const [messageStateArray, setMessageStateArray] = useState(INITIAL_CHAT_LOG);
+  const [messageStateArray, setMessageStateArray] = useState([]);
   const [currentUser, setCurrentUser] = useState(DEFAULT_USERS[0]) //initialize;
 
   const navigateTo = useNavigate(); //navigation hook
+
+
+  useEffect(() => {
+
+    const db = getDatabase();
+    const allMessagesRef = ref(db, "allMessages");
+
+
+    //when the database updates
+    onValue(allMessagesRef, (snapshot) => {
+      //get that data
+      const data = snapshot.val();
+
+      //take data obj -> array
+      const dataKeys = Object.keys(data);
+      const dataArray = dataKeys.map((key) => {
+        const transformed = data[key] 
+        return transformed;
+      })
+
+      //put it into state -> rerender
+      setMessageStateArray(dataArray);
+
+    })
+
+  }, []);
+
+
 
   //effect to run when the component first loads!
   useEffect(() => {
@@ -41,8 +71,19 @@ function App(props) {
       "timestamp": Date.now(),
       "channel": channel
     }
-    const newMessageArray = [...messageStateArray, newMessageObj];
-    setMessageStateArray(newMessageArray); //update state & rerender
+    // const newMessageArray = [...messageStateArray, newMessageObj];
+    // setMessageStateArray(newMessageArray); //update state & rerender
+
+    //instead of updating state, update database
+    const db = getDatabase();
+    const messageRef = ref(db, "message");
+    // firebaseSet(messageRef, newMessageObj);
+
+    const allMessagesRef = ref(db, "allMessages");
+    firebasePush(allMessagesRef, newMessageObj);
+
+
+
   }
 
   return (
--- react-chat-firebase-1/src/main.jsx
+++ react-chat-firebase-1/src/main.jsx
@@ -10,6 +10,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjMbEJRxWduNu-TdbQHYDfFmoKoOU4gHY",
+  authDomain: "react-chat-sp26.firebaseapp.com",
+  projectId: "react-chat-sp26",
+  storageBucket: "react-chat-sp26.firebasestorage.app",
+  messagingSenderId: "277212641268",
+  appId: "1:277212641268:web:8aaf7f88a14f2b012c6ba1"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
